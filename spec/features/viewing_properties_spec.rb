254fafd215be2310192dd8da0114f842f7872e05
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-20 13:57:05 +0100

    fixed feature test

--- spec/features/viewing_properties_spec.rb
+++ spec/features/viewing_properties_spec.rb
@@ -1,5 +1,10 @@
 feature 'viewing properties' do
   scenario 'user can see a list of properties' do
+    connection = PG.connect(dbname: 'makers_bnb_test')
+    connection.exec("INSERT INTO properties (name) VALUES('Number 2, Flower Lane');")
+    connection.exec("INSERT INTO properties (name) VALUES('The Penthouse');")
+    connection.exec("INSERT INTO properties (name) VALUES('Villa del Sol');")
+    
     visit '/listings'
     
     expect(page).to have_content 'Number 2, Flower Lane'
