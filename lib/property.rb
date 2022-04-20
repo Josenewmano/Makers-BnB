f0bac6b4038143baf7b8d91074291d1faed28307
Joe <j.newman@live.co.uk>
2022-04-20 11:59:26 +0100

    Failing when trying to implement basic database

--- lib/property.rb
+++ lib/property.rb
@@ -1,7 +1,10 @@
 class Property
+  attr_reader :name, :description, :price_per_night
 
   def self.all
-    ['Number 2, Flower Lane', 'The Penthouse', 'Villa del Sol']
+    @name = ['Number 2, Flower Lane', 'The Penthouse', 'Villa del Sol']
+    @description = ['A homely little cottage on a lane which lives up to its name, in a picture-postcard village', "Who wouldn't want to live in the top floor? Uber-stylish, uber-minimalist, uber-luxurious. Feel like you're on the top of the world, and actually be there!", 'Soak up the sun in your own little slice of paradise. Getting too hot? Slip into the private pool, or slip off your shirt if your prefer. Failing that, just follow the sea breeze and the sound of the waves to a perfectly secluded little cove']
+    @price_per_night = [125, 275, 225]
   end
 
 end
--- spec/property_spec.rb
+++ spec/property_spec.rb
@@ -3,6 +3,7 @@ require_relative '../lib/property'
 describe Property do
   describe '.all' do
     it 'shows a list of all properties' do
+      
       expect(Property.all).to include 'Number 2, Flower Lane'
     end
   end
