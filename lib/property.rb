345b49850bbd0e8038edaee0f3aff9777f125220
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-20 12:25:19 +0100

    basic db connection added

--- lib/property.rb
+++ lib/property.rb
@@ -1,10 +1,11 @@
+require 'pg'
 class Property
   attr_reader :name, :description, :price_per_night
 
   def self.all
-    @name = ['Number 2, Flower Lane', 'The Penthouse', 'Villa del Sol']
-    @description = ['A homely little cottage on a lane which lives up to its name, in a picture-postcard village', "Who wouldn't want to live in the top floor? Uber-stylish, uber-minimalist, uber-luxurious. Feel like you're on the top of the world, and actually be there!", 'Soak up the sun in your own little slice of paradise. Getting too hot? Slip into the private pool, or slip off your shirt if your prefer. Failing that, just follow the sea breeze and the sound of the waves to a perfectly secluded little cove']
-    @price_per_night = [125, 275, 225]
+    connection = PG.connect(dbname: 'makers_bnb')
+    result = connection.exec("SELECT * FROM properties;")
+    result.map { |property| property['name']}
   end
 
 end
--- spec/property_spec.rb
+++ spec/property_spec.rb
@@ -3,8 +3,8 @@ require_relative '../lib/property'
 describe Property do
   describe '.all' do
     it 'shows a list of all properties' do
-      
-      expect(Property.all).to include 'Number 2, Flower Lane'
+      properties = Property.all
+      expect(properties).to include 'Number 2, Flower Lane'
     end
   end
 end
