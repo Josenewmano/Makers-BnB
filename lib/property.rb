55966696ee1ada12b53c6efaa8fb6eb7702e1eb4
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2022-04-22 11:34:47 +0100

    refactored database in property.rb

--- lib/property.rb
+++ lib/property.rb
@@ -10,24 +10,12 @@ class Property
   end
 
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'makers_bnb_test')
-    else  
-      connection = PG.connect(dbname: 'makers_bnb')
-    end
-    
-    result = connection.exec("SELECT * FROM properties;")
-    result.map { |property| Property.new(id: property['id'], name: property['name'], description: property['description'], price_per_night: property['price_per_night'])
-  }
-  end
-
+    result = DatabaseConnection.query("SELECT * FROM properties")
+    result.map { |property| Property.new(id: property['id'], name: property['name'], description: property['description'], price_per_night: property['price_per_night']) }
+  end 
+  
   def self.create(name:, description:, price_per_night:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'makers_bnb_test')
-    else  
-      connection = PG.connect(dbname: 'makers_bnb')
-    end
-    result = connection.exec_params("INSERT INTO properties (name, description, price_per_night) VALUES($1, $2, $3) RETURNING id, name, description, price_per_night", [name, description, price_per_night])
+    result = DatabaseConnection.query("INSERT INTO properties (name, description, price_per_night) VALUES($1, $2, $3) RETURNING id, name, description, price_per_night", [name, description, price_per_night])
     Property.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], price_per_night: result[0]['price_per_night'])
   end
 
